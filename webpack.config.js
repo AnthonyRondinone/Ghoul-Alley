96c41e2f1d56b60ffa44e4c879b73e57d272469a
Anthony Rondinone <a.rondinone@gmail.com>
2017-09-07 17:27:15 -0400

    Successful file breakout

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+ context: __dirname,
+ entry: "./lib/game.js",
+ output: {
+   path: path.resolve(__dirname),
+   filename: "bundle.js"
+ },
+ module: {
+   loaders: [
+     {
+       test: [/\.jsx?$/, /\.js?$/],
+       exclude: /(node_modules)/,
+       loader: 'babel-loader',
+       query: {
+         presets: ['es2015']
+       }
+     }
+   ]
+ },
+ devtool: 'source-map',
+ resolve: {
+   extensions: [".js", ".jsx", "*"]
+ }
+};
